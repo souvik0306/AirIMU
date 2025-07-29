8b590a1746ef5c2fd5633121a0af7b241a550d33
souvik0306 <souvikdatta123@gmail.com>
2025-07-29 19:09:30 +0800

    Sys path fix

--- evaluation/evaluate_state.py
+++ evaluation/evaluate_state.py
@@ -1,6 +1,6 @@
 # output the trajctory in the world frame for visualization and evaluation
 import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
 import os
 import json
